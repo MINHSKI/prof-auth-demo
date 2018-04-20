e9c3dd8651c02640c52504e46a4138bcef4d4522
Eric P. Katz <ericpkatz@gmail.com>
2018-04-19 21:11:02 -0400

    commit

--- db/User.js
+++ db/User.js
@@ -1,9 +1,49 @@
 const conn = require('./conn');
 const { Sequelize } = conn;
+const jwt = require('jwt-simple');
+
+const KEY = process.env.JWT_KEY;
 
 const User = conn.define('user', {
   username: Sequelize.STRING,
   password: Sequelize.STRING
 });
+
+User.exchangeTokenForUser = function(token){
+  try {
+    const userId = jwt.decode(token, KEY).id;
+    return User.findById(userId)
+      .then( user => {
+        if(user)
+          return user;
+        throw {
+          status: 401
+        };
+      });
+  }
+  catch(ex){
+    return Promise.reject({
+      status: 401
+    });
+  }
+
+}
+
+User.authenticate = function(credentials){
+  const { username, password } = credentials;
+  return this.findOne({
+    where: {
+      username,
+      password
+    }
+  })
+  .then( user => {
+    if(user){
+      return jwt.encode({ id: user.id }, KEY);
+    }
+    throw { status: 401 };
+  });
+
+}
 
 module.exports = User;
--- test/server/models.spec.js
+++ test/server/models.spec.js
@@ -1,15 +1,83 @@
 const expect = require('chai').expect;
 const db = require('../../db');
-const User = {
-};
+const { User } = db.models;
+const jwt = require('jwt-simple');
 
+const KEY = process.env.JWT_KEY;
+
+let userMap;
 describe('models', ()=> {
   beforeEach(()=> {
-    return db.syncAndSeed();
+    return db.syncAndSeed()
+      .then( users => {
+        userMap = users.reduce((memo, user)=> {
+          memo[user.username] = user;
+          return memo;
+        }, {});
+      });
   });
   describe('User', ()=> {
     it('exists', ()=> {
       expect(User).to.be.ok;
+    });
+    it('moe exists', ()=> {
+      const moe = userMap.moe;
+      expect(moe.password).to.equal('MOE');
+    });
+  });
+  describe('User.authenticate', ()=> {
+
+    it('returns a token with correct credentials', ()=> {
+      const moe = userMap.moe;
+      const _token = jwt.encode({ id: moe.id }, KEY); 
+      const credentials = {
+        username: moe.username,
+        password: moe.password
+      };
+      return User.authenticate(credentials)
+        .then( token=> expect(token).to.equal(_token));
+    });
+
+    it('throws an error with a 401 status with bad credentials', ()=> {
+      const moe = userMap.moe;
+      const _token = jwt.encode({ id: moe.id }, KEY); 
+      const credentials = {
+        username: moe.username,
+        password: 'abc' 
+      };
+      return User.authenticate(credentials)
+        .catch( ex => expect(ex.status).to.equal(401));
+    });
+  });
+  describe('User.exchangeTokenForUser', ()=> {
+
+    it('returns a user with correct token', ()=> {
+      const moe = userMap.moe;
+      const _token = jwt.encode({ id: moe.id }, KEY); 
+      const credentials = {
+        username: moe.username,
+        password: moe.password
+      };
+      return User.authenticate(credentials)
+        .then( token=> User.exchangeTokenForUser(token))
+        .then( user => expect(user.username).to.equal(moe.username));
+    });
+
+    it('throws an error with a bad token', ()=> {
+      const larry = userMap.larry;
+      const token = jwt.encode({ id: larry.id }, 'what?'); 
+        return User.exchangeTokenForUser(token)
+        .catch( ex => expect(ex.status).to.equal(401));
+    });
+
+    it('throws an error with a good token with no user', ()=> {
+      const larry = userMap.larry;
+      const token = jwt.encode({ id: 99 }, KEY); 
+        return User.exchangeTokenForUser(token)
+         .then(()=> {
+           throw 'no';
+         })
+        .catch( ex => expect(ex.status).to.equal(401));
     });
   });
 });
